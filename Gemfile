3d8319a66a72148aaacf6af1022370def6eae613
Asami Takaoka <asamiyamabe61@gmail.com>
2021-07-05 22:38:59 +0900

    Userモデル,ゲストログイン機能実装

--- Gemfile
+++ Gemfile
@@ -56,8 +56,8 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pry-rails'
+gem 'devise'
 
-gem 'bootstrap-sass', '3.4.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   root 'static_pages#home'
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
 end
