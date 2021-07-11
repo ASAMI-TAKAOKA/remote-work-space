c4d8bd1e77b4a8110da5cf502149d06b6caa3854
Asami Takaoka <asamiyamabe61@gmail.com>
2021-07-12 07:39:49 +0900

    レイアウトの修正

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module RemoteWorkSpace
     # config.eager_load_paths << Rails.root.join("extras")
     # 認証トークンをremoteフォームに埋め込む
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    # タイムゾーンを日本時間に設定
+    config.time_zone = 'Tokyo'
+
   end
 end
