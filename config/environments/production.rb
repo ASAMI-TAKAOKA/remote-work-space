38fe8be245407dddff906c4a619b6743d67c44c5
Asami Takaoka <asamiyamabe61@gmail.com>
2021-07-24 04:35:33 +0900

    production.rb修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -125,7 +125,7 @@ Rails.application.configure do
     port: 587,
     user_name:            ENV['SEND_MAIL'],
     password:             ENV['SEND_MAIL_PASSWORD'],
-    authentication: 'plain'
+    authentication: 'plain',
     enable_starttls_auto:  true
   }
 end
