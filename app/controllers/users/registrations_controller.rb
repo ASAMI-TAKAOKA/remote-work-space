955b4511c0dc9487c43f0f39ee291041baca0fb9
Asami Takaoka <asamiyamabe61@gmail.com>
2021-07-25 03:48:34 +0900

    modified:devise

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,12 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :ensure_normal_user, only: :destroy
 
+  def create
+    super do                                             # 他はdeviseの機能をそのまま流用する
+      resource.update(confirmed_at: Time .now.utc)       # Welcomeメールを送信した上で、skip_confirmation!と同一処理を行い自動で認証クローズさせる
+    end
+  end
+
   def ensure_normal_user
     if resource.email == 'guest@example.com'
       　redirect_to root_path, danger: 'ゲストユーザーは削除できません。'
