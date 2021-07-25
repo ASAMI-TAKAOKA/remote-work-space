40ff25e69abcfbc85ec930044691da618c964a19
Asami Takaoka <asamiyamabe61@gmail.com>
2021-07-25 20:31:36 +0900

    modified:お問い合わせ機能

--- app/mailers/users/mailer.rb
+++ app/mailers/users/mailer.rb
@@ -3,10 +3,10 @@ class Users::Mailer < Devise::Mailer
   include Devise::Controllers::UrlHelpers
   default template_path: 'devise/mailer'
   def confirmation_instructions(record, token, opts={})
-    #record内にユーザ情報が入っていました。そこの"unconfirmed_email"の有無で登録／変更を仕分けます
-    #opts属性を上書きすることで、Subjectやfromなどのヘッダー情報を変更することが可能！
+    #record内にユーザ情報が入っていました。そこの"unconfirmed_email"の有無で登録／お問い合わせ受付を仕分けます
+    #opts属性を上書きすることで、Subjectやfromなどのヘッダー情報を変更することが可能
     if record.unconfirmed_email != nil
-      opts[:subject] = "【TELESPO】メールアドレス変更手続きを完了してください"
+      opts[:subject] = "【TELESPO】お問い合わせメールを受け付けました。"
     else
       opts[:subject] = "【TELESPO】会員登録完了"
     end
